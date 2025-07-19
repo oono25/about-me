4c3e67945d7c7d2b7e13690df0b0282495356b5d
ohno yuma <KTC25A31K0002@edu.kyoto-tech.ac.jp>
2025-07-19 16:11:59 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // すでに表示したかをlocalStorageで確認
+  const hasVisited = localStorage.getItem('hasVisited');
+
+  if (!hasVisited) {
     alert('自己紹介サイトへようこそ！');
     console.log('自己紹介サイトへようこそ！');
+    localStorage.setItem('hasVisited', 'true'); // 表示済みの印を保存
+  }
 });
