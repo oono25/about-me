51f1f25c28dbfa3cab4d4e01851c3491429eb951
noy1224 <KTC25A31K0002@edu.kyoto-tech.ac.jp>
2025-08-30 15:37:15 +0900

    Initial commit

--- submit.php
+++ submit.php
@@ -11,7 +11,7 @@ if (empty($name) || empty($email) || empty($message)) {
 }
 
 // メール送信（例）
-$to = "dayeyouzhen777@gmail.com"; // 管理者のメールアドレス
+$to = ""; // 管理者のメールアドレス
 $subject = "お問い合わせがありました";
 $body = "名前: $name\nメール: $email\n内容:\n$message";
 $headers = "From: $email";
